b5d093600220ef0a3df5c809c4cd5b3f63be90b9
EriCrysBel <erikacrystal95@gmail.com>
2024-03-21 09:42:32 +0100

    parte 2: uso y estilos de los componentes React

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout'; 
 
-const Blog = function ()
+const Blog = function (){
+  return(
+    <Layout pageTitle={'Mi Blog'}>
+      <p>Aqui estará mi Blog proximamente</p>
+    </Layout>
+  )
+}
+
+export default Blog
