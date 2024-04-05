9967a162e4f821eb1e98f902545c86489263acd8
EriCrysBel <erikacrystal95@gmail.com>
2024-04-05 10:51:12 +0200

    fin de la parte 5

---
+++ src/pages/blog.js
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+export const query = graphql`
+    query {
+        allMdx {
+            nodes {
+                frontmatter {
+                title
+                date
+            }
+            excerpt
+        }
+        }
+}
+`
+const Blog = ({data}) =>{
+    const nombre = "Doraemon"
+    
+    return (
+        <Layout pageTitle='Blog'>
+        {data.allMdx.nodes.map(node => 
+        <div>
+        <p>{node.frontmatter.date}</p>
+        <h2>{node.frontmatter.title}</h2>
+        <p>{node.excerpt}</p>
+        
+        <hr>
+        </hr>
+        </div> 
+        )}
+        </Layout>
+    )
+}
+
+export const Head = () => <Seo title="Blog" />
+export default Blog
